733cee9567ecc8d391716e982f135cedc7fcf796
lukebiao <2363423124@qq.com>
2017-10-12 23:21:15 +0800

    first commit

---
+++ springhelloworld/src/main/java/com/example/springhelloworld/SpringhelloworldApplication.java
@@ -0,0 +1,21 @@
+package com.example.springhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringhelloworldApplication {
+
+	@RequestMapping("/")
+	public String helloWorld() {
+		return "hello world!";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhelloworldApplication.class, args);
+	}
+}
